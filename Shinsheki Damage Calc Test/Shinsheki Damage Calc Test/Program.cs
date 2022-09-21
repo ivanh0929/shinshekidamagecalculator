4aef9f6d664dc2db3d5491e1aadbf3c29c720995
Ivan Hernandez (RIT Student) <iah7731@rit.edu>
2022-09-21 09:08:55 -0400

    Initial commit
    Did the design for the damage calculator.

---
+++ Shinsheki Damage Calc Test/Shinsheki Damage Calc Test/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Name: Shinsheki Damage Calculations
+ * Creator: Ivan Hernandez
+ * Purpose: make a test program for Shinsheki damage calculations to practice methods
+ * Date: 9/21/222
+ * Modification:
+*/
+
+namespace ShinshekiDamageCalcer // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome to Damage Calcer
+            //Get all of the variables (Weapon Power, Strength Stat, Enemy Defense, Enemy Armor, Skill Power, Magic Stat)
+            //What formula you want boss
+            //Make a method for each Formula (Fight Command, Physical Skill, Magic Skill)
+            //Send the needed information to the correct method
+            //Give the final damage number
+            //Write all of the methods on the bottom
+
+        }
+    }
+}
